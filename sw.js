05c0d89f2863748cf775c602146dff1b9ca46e3e
Bot <y@y.com>
2026-04-17 14:06:44 +0000

    revert to original design + fix viewport only

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v12';
+const CACHE='yeshiva-v13';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
