ce0297f0236b2b0f401b5f5b4d081f6e4c5b81cb
Dev <dev@yeshiva.com>
2026-04-19 16:11:53 +0000

    mobile: כפיית layout מובייל כש-apk=1 בURL

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v15';
+const CACHE='yeshiva-v16';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
