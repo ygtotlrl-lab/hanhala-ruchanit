7da995875bff9e6e63afb94940d4d4978f240856
Dev <dev@yeshiva.com>
2026-04-24 12:02:24 +0000

    תיקון charset: הנהלה רוחנית בטאב הדפדפן

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v19';
+const CACHE='yeshiva-v20';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
