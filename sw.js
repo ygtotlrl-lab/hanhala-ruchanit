ca35ccf8448f8133cf42938e09a8f9d9359b9a1b
Dev <dev@yeshiva.com>
2026-04-25 19:42:40 +0000

    auth: מעבר לסיסמה plaintext - פשוט ואמין

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v24';
+const CACHE='yeshiva-v25';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
