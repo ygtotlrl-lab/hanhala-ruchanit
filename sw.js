5f845373a8a5abd4fd540fb037eab636ca919889
Dev <dev@yeshiva.com>
2026-04-25 19:18:36 +0000

    debug: הצג hash בשגיאת login

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v21';
+const CACHE='yeshiva-v22';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
