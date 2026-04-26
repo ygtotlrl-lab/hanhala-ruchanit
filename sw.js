829a30b2b8630f11f0d11d7e69c468dc9b5306bf
Dev <dev@yeshiva.com>
2026-04-26 14:50:54 +0000

    fix: renderSettings ללא גישה לאלמנטים מוסתרים - SW v64

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v63';
+const CACHE='yeshiva-v64';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
