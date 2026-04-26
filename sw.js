bdf66ab6deda918b35d3787df797c66dba3ca0a3
Dev <dev@yeshiva.com>
2026-04-26 09:27:00 +0000

    fix: hdr-username עם fallback - SW v44

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v43';
+const CACHE='yeshiva-v44';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
