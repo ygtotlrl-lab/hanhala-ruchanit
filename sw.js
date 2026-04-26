1e9f0f143a9ba768f93069fe21df4bb85793ea65
Dev <dev@yeshiva.com>
2026-04-26 06:10:55 +0000

    header: הצג טקסט ב"ה במובייל - SW v32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v31';
+const CACHE='yeshiva-v32';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
