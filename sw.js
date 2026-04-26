170a59897e315b6e63138a145d3e9b85a4b387d1
Dev <dev@yeshiva.com>
2026-04-26 03:17:22 +0000

    fix CRITICAL: פיצול script tag - תיקון SyntaxError שורה 14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v26';
+const CACHE='yeshiva-v27';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
