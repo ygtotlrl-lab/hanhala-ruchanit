af3c92ee2570f3d1a63effed1c3e67e173a275e8
Dev <dev@yeshiva.com>
2026-04-26 09:32:58 +0000

    feat: avatar button עם dropdown - SW v45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v44';
+const CACHE='yeshiva-v45';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
