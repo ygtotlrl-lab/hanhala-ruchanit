e3fbc0e478558dc5ce20da66965251d2c537541a
Dev <dev@yeshiva.com>
2026-04-26 11:18:24 +0000

    feat: החלפת משתמש מהתפריט - SW v51

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v50';
+const CACHE='yeshiva-v51';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
