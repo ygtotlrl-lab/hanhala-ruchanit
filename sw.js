bebb66ad9f18a2e186ca52212ca0955effd43500
Dev <dev@yeshiva.com>
2026-04-26 14:27:04 +0000

    fix: הסרת צביעת שורות לפי סטטוס - SW v60

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v59';
+const CACHE='yeshiva-v60';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
