1c413eb105ceeca9aeaa5480a1d1590a2c16f745
Dev <dev@yeshiva.com>
2026-04-26 14:06:55 +0000

    fix: nowrap בנוכחות + סמל + בסטטוס - SW v59

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v58';
+const CACHE='yeshiva-v59';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
