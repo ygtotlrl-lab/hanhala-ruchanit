8f7cf3b671c1ca2d7cb1f5fe417e834ecf904d2b
Dev <dev@yeshiva.com>
2026-04-26 14:58:56 +0000

    fix: showSettingsModule עם setTimeout - SW v65

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v64';
+const CACHE='yeshiva-v65';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
