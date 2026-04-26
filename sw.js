14748ca5ba9f46df1f76a521443dfc5c751ffbf8
Dev <dev@yeshiva.com>
2026-04-26 10:25:22 +0000

    feat: כפתור משתמש בסגנון האפליקציה - SW v50

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v49';
+const CACHE='yeshiva-v50';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
