c3684efac1e0b93cd72a474ff7fa3f1b3ec84463
Dev <dev@yeshiva.com>
2026-04-26 06:27:11 +0000

    fix: construction modal דינמי - SW v34

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v33';
+const CACHE='yeshiva-v34';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
