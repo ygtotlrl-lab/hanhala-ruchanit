b8e62047f5d45e1f4468cbcfbdd8689e5c025780
Dev <dev@yeshiva.com>
2026-04-26 09:07:32 +0000

    fix: סדר משתמשים ב-localStorage במקום DB - SW v42

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v41';
+const CACHE='yeshiva-v42';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
