935eff824234227c194574632b1e17b0f0a7157d
Dev <dev@yeshiva.com>
2026-04-26 12:19:05 +0000

    feat: הפרדת נוכחות (תצוגה) מסטטוס (עריכה) - SW v55

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v54';
+const CACHE='yeshiva-v55';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
