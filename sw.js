e3f6d977a0d1d1c5f35985ee664f9aef04b4d030
Dev <dev@yeshiva.com>
2026-04-26 14:29:57 +0000

    feat: החלפת עיפרון ב-☰ לעריכת תלמיד - SW v61

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v60';
+const CACHE='yeshiva-v61';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
